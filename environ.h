056e56aad4b21ae27d9c551ba6e6c1be4fb67c91
Michel Massamiri <michel-massamiri@etu.u-bordeaux.fr>
2017-03-14 23:35:02 +0100

    supprimer les enums

--- environ.h
+++ environ.h
@@ -22,22 +22,3 @@ extern int valch(ENV rho, char *var); /* valeur de var dans envirnt rho         
 /* ------------------CONSTANTES -------------------------------------------------*/
 #define MAXIDENT 16          /* long max d'un identificateur de variable         */
 #define MAXQUAD  5*MAXIDENT  /* long max d'un quadruplet c3a                     */
-
-#define Pl 257
-#define Mo 258
-#define Mu 259
-#define Af 260
-#define Afc 261
-#define Sk 262
-#define Jp 263
-#define Jz 264
-#define St 265
-
-#define If 266
-#define Th 267
-#define El 268
-#define Wh 269
-#define Do 270
-#define Se 271
-#define NUMBER 272
-#endif
