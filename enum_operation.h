a5095703badee90517f6518e857711bc184b4cc4
Michel Massamiri <michel-massamiri@etu.u-bordeaux.fr>
2017-03-14 23:32:36 +0100

    mettre les enums dans un autre fichier .h

---
+++ enum_operation.h
@@ -0,0 +1,22 @@
+#ifndef ENUM_OPERATION_H
+#define ENUM_OPERATION_H
+
+#define Pl 257
+#define Mo 258
+#define Mu 259
+#define Af 260
+#define Afc 261
+#define Sk 262
+#define Jp 263
+#define Jz 264
+#define St 265
+
+#define If 266
+#define Th 267
+#define El 268
+#define Wh 269
+#define Do 270
+#define Se 271
+#define NUMBER 272
+
+#endif
